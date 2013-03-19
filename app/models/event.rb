8c1995ff3e7e3dbf93b2f6d6187a2231a1c665c9
Owen K. <kuemerle5@hotmail.com>
2013-03-19 17:00:53 -0500

    Finalized models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :datetime, :poster, :taken_datetime, :taker
+
+  belongs_to :poster, class_name: 'User', foreign_key: 'poster'
+  belongs_to :taker, class_name: 'User', foreign_key: 'taker'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :role
+
+  has_many :events, foreign_key: 'poster'
+  has_many :events_taken, class_name: 'Event', foreign_key: 'taker'
 end
