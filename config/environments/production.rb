71be5b0debf1da188e816cbadd16e3c911340465
Owen K. <kuemerle5@hotmail.com>
2013-04-11 09:43:13 -0500

    Try precompiling again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Takeit::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile << '*.js'
+  config.assets.precompile = ['*.js', '*.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
