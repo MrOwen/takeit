283121741508ac3f4b98afd5be83789b1e166571
Owen K. <kuemerle5@hotmail.com>
2013-04-11 09:32:03 -0500

    Add new production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,8 @@ Takeit::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :warn
+  config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
@@ -60,8 +61,4 @@ Takeit::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
