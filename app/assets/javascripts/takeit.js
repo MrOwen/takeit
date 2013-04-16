2fa75d442cef9c7c0a90338537e36ab5a2c05648
Owen K. <kuemerle5@hotmail.com>
2013-04-16 08:56:22 -0500

    Making additions to calendar view

--- app/assets/javascripts/takeit.js
+++ app/assets/javascripts/takeit.js
@@ -3,39 +3,36 @@
 //---------------
 
 //CalCtrl controller deals with the calendar dates
-takeIt.controller('CalCtrl', function CalCtrl($scope) {
-	var days = $scope.days = jQuery.parseJSON($.ajax("/cal_data", {
-		dataType: "json",
-		async: false
-	}).responseText).calendar_days;
+angular.module('takeIt.controllers', []).
+	controller('CalCtrl', function ($scope, $http) {
+		$http.get('/cal_data.json').success(function(data) {
+			days = $scope.days = data.calendar_days;
+			shifts = $scope.shifts = data.shifts;
+			console.log(shifts);
+		});
 
-	var shifts = $scope.shifts = jQuery.parseJSON($.ajax("/cal_data", {
-		dataType: "json",
-		async: false
-	}).responseText).shifts;
+		$scope.shiftsExtract = function(date) {
+			return shifts[date];
+		};
 
-	$scope.shiftsExtract = function(date) {
-		return shifts[date];
-	};
+		$scope.dateExtract = function(date) {
+			return moment(date).date();
+		};
 
-	$scope.dateExtract = function(date) {
-		return moment(date).date();
-	};
-
-	$scope.buttonColorer = function(shift) {
-		// console.log(moment(shift.start_date).subtract('days', 1).hour());
-		// if (shift.taker === null && moment().from(moment(shift.start_date).subtract('days', 1)).hour() < 24) {
-		// 	// console.log("The shift date is: " + moment(shift.start_date).format() + " and +1 day from that is: " + moment(shift.start_date).add('days', 1).format());
-		// 	// console.log("The shift taker is: " + shift.taker);
-		// 	return "btn-primary";
-		// } else if (shift.taker === null) {
-		// 	return "btn-warning";
-		// } else {
-		// 	return "btn-success";
-		// }
-		return "btn-primary";
-	};
-});
+		$scope.buttonColorer = function(shift) {
+			console.log(moment(shift.start_date).subtract('days', 1).hour());
+			if (shift.taker === null && moment().from(moment(shift.start_date).subtract('days', 1)).hour() < 24) {
+				// console.log("The shift date is: " + moment(shift.start_date).format() + " and +1 day from that is: " + moment(shift.start_date).add('days', 1).format());
+				// console.log("The shift taker is: " + shift.taker);
+				return "btn-primary";
+			} else if (shift.taker === null) {
+				return "btn-warning";
+			} else {
+				return "btn-success";
+			}
+			return "btn-primary";
+		};
+	});
 
 // //ShiftCtrl deals with shift data
 // takeIt.Controller('ShiftCtrl', function ShiftCtrl($scope) {
@@ -54,7 +51,6 @@ angular.module('takeIt.filters', []).
 			angular.forEach(dates, function(date) {
 				extractedDates.push(moment(date, "YYYY-MM-DD").date());
 			}, extractedDates);
-			console.log(extractedDates);
 			return extractedDates;
 		};
 	});
@@ -62,6 +58,60 @@ angular.module('takeIt.filters', []).
 //---------------
 // Directives
 //---------------
+angular.module('takeIt.directives', []).
+	directive('popoverHandler', function(){
+		var linker = function(scope, element, attrs) {
+			console.log("The linker was called");
+
+			$("a[data-toggle=popover]").popover({
+				html: true,
+				placement: "right",
+				trigger: "manual",
+				container: "body"})
+			//This prevents the browser from scrolling to the top on an empty anchor (#)
+			.click(function(e) {
+				e.preventDefault();
+			});
+
+			$("a[data-toggle=popover]").click(function() {
+				var popoverButton = $(this);
+				$(this).data('isActive', true);
+
+				//Don't re-show the popover animation if it's already visible
+				if(!$(".popover.fade.right.in").is(":visible")) {
+					popoverButton.popover('show');
+				}
+				clearTimeout(popoverButton.data('timeoutId'));
+			}).mouseout(function() {
+				var popoverButton = $(this);
+				//A timer used to hide the popover after some time
+				//Attached to the button as a function (.data())
+				var timeoutId = setTimeout(function() {
+					popoverButton.popover('hide');
+				}, 750);
+				//Start the timer to hide the popover because the mouse left the popover element
+				popoverButton.data('timeoutId', timeoutId);
+				$(".popover.fade.right.in").hover(function() {
+					clearTimeout(popoverButton.data('timeoutId'));
+				}, function() {
+					//For whatever reason, timeoutId needs to be redefined in this scope
+					var timeoutId = setTimeout(function() {
+						popoverButton.popover('hide');
+					}, 750);
+					popoverButton.data('timeoutId', timeoutId);
+				});
+			});
+
+			$("body").on("mouseenter", ".popover.fade.right.in", function() {
+				clearTimeout($("a[data-toggle=popover]").data('timeoutId'));
+			});
+		};
+
+		return {
+			restrict: 'C',
+			link: linker
+		};
+	});
 
 
 //---------------
