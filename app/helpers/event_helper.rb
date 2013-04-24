2b42def9662c80fc5d5c71403b073276867f7a99
Owen K. <kuemerle5@hotmail.com>
2013-04-24 09:02:05 -0500

    Add shift id and date posted fields to json shift data and modify the attr_accessible in events model

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -2,7 +2,7 @@ module EventHelper
 	require 'date'
 	# end_date is for future use if more or less than a 2 week span needs to be calculated
 	#For now, it's not used, so it can just be set to nil by default
-	def json_days_shifts(start_date, end_date = nil)
+	def json_event_builder(start_date, end_date = nil)
 		setup = {}
 		shifts_array = {}
 		curr_date = start_date - start_date.wday
@@ -12,15 +12,21 @@ module EventHelper
 			curr_date = Date.rfc3339(e.start_datetime).to_s
 			# If the current event's date isn't an element in the shifts array, we create and populate it
 			(shifts_array[curr_date].nil? ? shifts_array[curr_date] = [] : shifts_array[curr_date]) << {
+				shift_id: e.id,
 				start_date: e.start_datetime,
 				end_date: e.end_datetime,
 				date_taken: e.taken_datetime,
+				date_posted: e.created_at,
 				poster: e.poster.nil? ? nil : e.poster.name,
 				taker: e.taker.nil? ? nil : e.taker.name
 			}
 		end
 		setup[:shifts] = shifts_array
 		setup[:calendar_days] = days_array
+		setup[:user_data] = {
+			name: current_user.name,
+			email: current_user.email
+		}
 		setup.to_json.html_safe
 	end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :start_datetime, :end_datetime, :poster, :taken_datetime, :taker, :poster
+  attr_accessible :start_datetime, :end_datetime, :poster, :taker, :taken_datetime
 
   belongs_to :poster, class_name: 'User', foreign_key: 'poster'
   belongs_to :taker, class_name: 'User', foreign_key: 'taker'
