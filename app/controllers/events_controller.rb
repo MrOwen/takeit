143c60456e34fc018e420535e5b53d39ea9d9453
Owen K. <kuemerle5@hotmail.com>
2013-04-24 09:00:55 -0500

    Better formatting of the cell contents and popover partials

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,43 @@
 class EventsController < ApplicationController
 	respond_to :json, :html
+
 	def calendar
+	end
+
+	# GET /events/new.json
+	# def new
+	# 	@event = Event.new(params[:event])
+
+	# 	respond_to do |format|
+	# 		format.json { render json: @event }
+	# 	end
+	# end
+
+	# POST /events.json
+	def create
+		params[:event][:poster] = User.find_by_email(params[:event][:poster])
+		@event = Event.new(params[:event])
+
+		respond_to do |format|
+			if @event.save
+				format.json { render json: @event, status: :created, location: @event }
+			else
+				format.json { render json: @event.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	# PUT /orders/1.json
+	def update
+		params[:event][:taker] = User.find_by_email(params[:event][:taker])
+		@event = Event.find(params[:id])
+
+		respond_to do |format|
+			if @event.update_attributes(params[:event])
+				format.json { head :no_content }
+			else
+				format.json { render json: @event.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 end
